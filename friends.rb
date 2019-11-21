d0fee4047a5d45a44d61fa2a7243860321a691f3
James Fraser <jimbob4t@virginmedia.com>
2019-11-21 15:47:52 +0000

    question 3 completed

--- friends.rb
+++ friends.rb
@@ -5,3 +5,12 @@ end
 def get_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, food)
+  for snack in person[:favourites][:snacks]
+  if  snack == food
+    return true
+  end
+end
+    return false
+end
